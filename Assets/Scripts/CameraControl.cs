84d1a3c2c44f7bb63fb34225fc28f399881aca43
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-14 13:16:38 +0700

    Greybox

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public Transform player;
+    public float smoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, 15, player.position.z);
+            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+        }
+    }
+}
---
+++ Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveZ = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveX, 0, moveZ).normalized * speed;
+        rb.linearVelocity = movement;
+    }
+}
